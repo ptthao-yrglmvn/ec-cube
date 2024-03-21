64880be515ee928126d0201d4a7b394f7114d771
lqquang <lqquang@yrglm-vn.com>
2024-03-21 08:32:21 +0700

    Create test case for New Plugin in new Ec-cube version 4.0

---
+++ codeception/_data/plugin_fixtures_v2.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// return [
+//     "authen_key"=> 'ea3f19f7cc03deeda955bedb53a4b8db6dd6e16a',
+//     "name" => 'お気に入り登録商品ブロック追加プラグイン for EC-CUBE4'    
+// ];
+
+return [
+    "authen_key"=> 'ea3f19f7cc03deeda955bedb53a4b8db6dd6e16a',
+    "name" => 'お気に入り商品表示ブロック'    
+];
