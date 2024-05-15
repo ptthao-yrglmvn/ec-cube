431f769ebcdc686ccb189ab7662551d05f0d5732
Pham Thanh Thao <ptthao@yrglm-vn.com>
2024-05-15 16:11:40 +0700

    remember login session

--- codeception/_support/AcceptanceTester.php
+++ codeception/_support/AcceptanceTester.php
@@ -51,6 +51,9 @@ class AcceptanceTester extends \Codeception\Actor
         }
 
         $I = $this;
+        if ($I->loadSessionSnapshot('login')) {
+            return;
+        }
         $this->goToAdminPage($dir);
 
         $I->submitForm('#form1', [
@@ -59,6 +62,7 @@ class AcceptanceTester extends \Codeception\Actor
         ]);
 
         $I->see('ホーム', '.c-contentsArea .c-pageTitle > .c-pageTitle__titles');
+        $I->saveSessionSnapshot('login');
     }
 
     public function logoutAsAdmin()
